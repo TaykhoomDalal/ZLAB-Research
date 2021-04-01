9204f426e2e382a9023bb05f139b3fcfc046dc91
Taykhoom Dalal <tdalal@g.ucla.edu>
2021-03-31 17:07:03 -0700

    updated Association script

--- GWAS_Scripts/Assoc_QSUB_Script.sh
+++ GWAS_Scripts/Assoc_QSUB_Script.sh
@@ -2,17 +2,18 @@
 #!/bin/bash
 #$ -cwd
 # error = Merged with joblog
-#$ -o assoc_samples_error_log.$JOB_ID
+#$ -o assoc_samples_error_log.$JOB_ID.$TASK_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=24:00:00,h_data=24G
+#$ -l h_rt=12:00:00,h_data=8G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -pe shared 1
+#$ -pe shared 11
 # Email address to notify
 #$ -M $USER@mail
 # Notify when
 #$ -m bea
+#$ -t 11:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -27,7 +28,21 @@ module load plink
 ## substitute the command to run your code
 ## in the two lines below:
 echo '/usr/bin/time -v hostname'
-./SampleFULL_GWAS.sh
+
+./plink2 --bgen ~/project-ukbiobank/data/geno/imp/bgen/ukb_imp_chr"$SGE_TASK_ID"_v3.bgen \
+--sample ~/project-ukbiobank/data/geno/imp/bgen/impv3.sample \
+--threads 11 \
+--memory 88000 \
+--extract ~/project-zaitlenlab/dataFolder/info_maf_qc_ukb_mfi_chr"$SGE_TASK_ID"_v3.txt \
+--geno 0.05 \
+--hwe 1e-10 \
+--maf 0.001 \
+--glm \
+--pheno ~/project-ukbiobank/33127/ukb21970_plink/filter4_pheno_files/height.pheno \
+--covar ~/project-ukbiobank/33127/ukb21970_plink/filter4_pheno_files/height.covar \
+--covar-name sex,f.22009.0.1,f.22009.0.2,f.22009.0.3,f.22009.0.4,f.22009.0.5,f.22009.0.6,f.22009.0.7,f.22009.0.8,f.22009.0.9,f.22009.0.10 \
+--parameters 1 \
+--out /u/scratch/t/taykhoom/assoc_chr"$SGE_TASK_ID"_v3
 
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
